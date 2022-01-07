e4e7d4c38fbcad73350b02f0983e31be51cf8007
theodore cleveland <ted.cleveland@gmail.com>
2022-01-07 17:24:34 +0000

    update lesson7

---
+++ ce3372-jb/ce3372jb/_build/jupyter_execute/lessons/lesson07/lesson07.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Lesson 7. Storage Systems
+# 
+# Storage is used in water supply, storm water management, and wastewater systems for several reasons. 
+# 
+# ## Scale
+# 
+# There are several scales of storage: 
+# - large (water supply reservoirs, like Lake Allen Henry)
+# <figure>
+# <!--<img src="http://54.243.252.9/ce-3372-webroot/ce3372-jb/ce3372jb/lessons/lesson07/LakeAlanHenry.png" width="400">-->
+# <img src="LakeAlanHenry.png" width="600"></figure>
+# - moderate (Southwest Lubbock Water Treatment)
+# <figure>
+# <!--<img src="http://54.243.252.9/ce-3372-webroot/ce3372-jb/ce3372jb/lessons/lesson07/LBB-SDWPlant.png" width="400">--><img src="LBB-SDWPlant.png" width="500">
+# - localized (storage tanks,  )
+# <figure>
+# <!--<img src="http://54.243.252.9/ce-3372-webroot/ce3372-jb/ce3372jb/lessons/lesson07/WaterTower.png" width="400">--><img src="WaterTower.png" width="400">
+# </figure>
+# 
+# The large scale systems are sized using water resources management principles, which are discussed to some extent in Gupta pp. 545-548.  The usual tool is called a ripple-chart or double mass curve (same thing, different names).  In local scale an identical tool is used, but at a much different (hourly) time-scale.
+#     
+
+# ---
+# 
+# ## Local Scale
+# 
+# The remainder of this chapter will focus on the local scale systems.  At the local scale (Gupta pp. 548-553) we are concerned with three categories of storage:
+#     
+# **SERVICE STORAGE**  
+# 1. Flow equalization — generally things are designed for a particular steady flow rate and storage can be used to accommodate variable flow rates in a system. 
+# 2. Pressure zone maintenance 
+# 
+# **EMERGENCY STORAGE**  
+# 1. used to provide supply during repairs and other system interrupts (except fire suppression)
+# 
+# **FIRE STORAGE**  
+# 1. used to provide supply during fire fighting activities – considered seperately from emergency storage
+
+# 
+
+# ## References
+# 1. Gupta, R. S. 2017. Hydrology and Hydraulic Systems. Waveland Press, Inc. pp. 545-553
+# 
+# 2. Roberson, J.A., Cassidy, J.J., and Chaudry, M.H. (1988) Closed Conduits in "Hydraulic Engineering." 
+# Houghton Mifflin Co. pp. 240-310 [http://54.243.252.9/ce-3372-webroot/3-Readings/Chapter5/](http://54.243.252.9/ce-3372-webroot/3-Readings/Chapter5/)
+# 
+# 
+# 3. Chin, D. (2006). pp. 10-26 in "Water Resources Engineering, 2 ed." Prentice Hall, Inc. [http://54.243.252.9/ce-3372-webroot/3-Readings/Chin_10-26/](http://54.243.252.9/ce-3372-webroot/3-Readings/Chin_10-26/)
+# 
+# 
+# 4. Notes on Hydraulics  Work in progress -- largely replicates content in "Closed Conduit Flow" with some extensions
+# [http://54.243.252.9/ce-3372-webroot/3-Readings/HydraulicsNotes/](http://54.243.252.9/ce-3372-webroot/3-Readings/HydraulicsNotes/)
+# 
+# 5. "Water Distribution Systems" in Land Development Handbook, Ed. S.O. Dewberry, Dewberry Inc., McGraw-Hill
+# [http://54.243.252.9/ce-3372-webroot/3-Readings/WaterDistributionSystems/](http://54.243.252.9/ce-3372-webroot/3-Readings/WaterDistributionSystems/)
+
+# In[ ]:
+
+
+
+
